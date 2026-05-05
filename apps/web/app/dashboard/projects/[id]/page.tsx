2331abbf840d2b65ea7f47a43969c179bdc5575e
febriarr <febriardiansyah020227@gmail.com>
2026-05-06 05:13:35 +0700

    fix(web): fix hardcode api url

--- apps/web/app/dashboard/projects/[id]/page.tsx
+++ apps/web/app/dashboard/projects/[id]/page.tsx
@@ -1,11 +1,13 @@
 import { DetailProjectPage } from "@/app/dashboard/projects/[id]/_components/projects-detail"
 
+export const dynamic = "force-dynamic"
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
 
   const [categoryRes, techStacksRes] = await Promise.all([
-    fetch("http://localhost:8000/api/project-categories"),
-    fetch("http://localhost:8000/api/tech-stacks"),
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/project-categories`),
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/tech-stacks`),
   ])
 
   const categoryOptions = await categoryRes.json()
--- apps/web/app/dashboard/projects/page.tsx
+++ apps/web/app/dashboard/projects/page.tsx
@@ -1,8 +1,10 @@
 import { ProjectsPage } from "@/app/dashboard/projects/_components/projects-page"
 
+export const dynamic = "force-dynamic"
+
 export default async function Page() {
-  const category = await fetch("http://localhost:8000/api/project-categories")
-  const techStacks = await fetch("http://localhost:8000/api/tech-stacks")
+  const category = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/project-categories`)
+  const techStacks = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tech-stacks`)
 
   const categoryOptions = await category.json()
   const techStackOptions = await techStacks.json()
