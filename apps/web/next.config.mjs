57899f76e4d1dc903dc71a25bb15603eaaa2e58e
febriarr <febriardiansyah020227@gmail.com>
2026-05-06 04:39:36 +0700

    fix(web): fix next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@workspace/validator", "@workspace/ui", "@workspace/shared"],
   images: {
     remotePatterns: [
       {
