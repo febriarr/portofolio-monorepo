581417f23062b3f13fde10786879b6e0da314adf
febriarr <febriardiansyah020227@gmail.com>
2026-05-16 12:24:02 +0700

    fix(blog): add variable dynamic force-dynamic

--- apps/blog/src/app/sitemap.ts
+++ apps/blog/src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const payload = await getPayload({
     config: configPromise,
