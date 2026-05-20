3cd660266988ce7b29b50e66a14945c3eda2b2f3
febriarr <febriardiansyah020227@gmail.com>
2026-05-21 04:08:33 +0700

    fix: fix sitemap at apps/web

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -4,9 +4,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.febriardiansyah.my.id",
-      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: "https://www.febriardiansyah.my.id/about",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.febriardiansyah.my.id/projects",
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.febriardiansyah.my.id/contact",
+      changeFrequency: "yearly",
+      priority: 0.5,
     },
   ]
 }
